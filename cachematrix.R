35d9bf4b4a1f900924fd67c1ce2ddbea41a2b37b
Jinal Kothari <kjinal07@gmail.com>
2020-05-24 18:54:59 +0530

    Added comments to both functions.

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# this function defines functions used for storing the value of x, retrieving the value of x,
+# storing the inverse of x and retrieving inverse of x
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
     set <- function(y) {
@@ -19,7 +20,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
         
 ## Write a short comment describing this function
-
+# this function first checks if the inverse of x was already calculated or not.
+# If the inverse was already calculated then it is retrieved from the stored variable
+# If the inverse was not previously calculated then inverse is calculated using solve function
+# and the inverse is also stored in a variable for future use. 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
     m <- x$getinverse()
